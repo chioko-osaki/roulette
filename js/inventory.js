3facd7e512de66fa3e0ac699ee76a6b13966529f
chioko-osaki <hashimoto.digioosaki@gmail.com>
2026-09-14 16:23:31 +0900

    Update inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -12,9 +12,12 @@
    ========================================================= */
 
 const PRIZE_CONFIG = [
-  { id: 'A', name: 'A賞', image: 'images/prize-a.png', initialStock: 5 },
-  { id: 'B', name: 'B賞', image: 'images/prize-b.png', initialStock: 10 },
-  { id: 'C', name: 'C賞', image: 'images/prize-c.png', initialStock: 20 },
+ { id: 'A', name: '【橋元隊員】特製スマホクリーナー', image: 'images/prize-a.png', initialStock: 20 },
+ { id: 'B', name: '【秋山隊員】○○', image: 'images/prize-b.png', initialStock: 20 },
+ { id: 'C', name: '【氏家隊員】○○', image: 'images/prize-c.png', initialStock: 20 },
+ { id: 'D', name: '【外崎隊員】○○', image: 'images/prize-c.png', initialStock: 20 },
+ { id: 'E', name: '【粕谷隊員】○○', image: 'images/prize-c.png', initialStock: 20 },
+ { id: 'F', name: '【今野隊員】○○', image: 'images/prize-c.png', initialStock: 20 },
 ];
 
 const Inventory = (function () {
